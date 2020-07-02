f3cd2655145ccfbb05b721f546cf9fb6ab6dfbf3
shahin jafarzadeh <you@example.comjafarzadehshahin@gmail.com>
2020-07-02 16:07:34 +0200

    added insert and post fucntion

--- app.py
+++ app.py
@@ -1,12 +1,32 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'cook-book'
+app.config["MONGO_URI"] = 'mongodb+srv://iixshahinxii:Shahin1371@firstcluster-4kc0v.mongodb.net/cook-book?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
+
 
 @app.route('/')
-def hello():
-    return 'hello guys'
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template("recipes.html", recipes=mongo.db.recipes.find())
+
+
+@app.route('/add_recipe')
+def add_recipe():
+    return render_template('addrecipe.html')
+
+
+@app.route('/insert_recipe', methods=['POST'])
+def insert_recipe():
+    recipes = mongo.db.recipes
+    recipes.insert_one(request.form.to_dict())
+    return redirect(url_for('get_recipes'))
 
 
 if __name__ == '__main__':
