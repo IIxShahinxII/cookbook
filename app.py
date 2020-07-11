dbfb00ca9789e665d26123164525bd35654a2999
IIxShahinxII <34623753+IIxShahinxII@users.noreply.github.com>
2020-07-11 02:01:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from bson.objectid import ObjectId
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'cook-book'
-app.config["MONGO_URI"] = ****
+app.config["MONGO_URI"] = 'mongodb+srv://iixshahinxii:Shahin1371@firstcluster-4kc0v.mongodb.net/cook-book?retryWrites=true&w=majority'
 
 mongo = PyMongo(app)
 
